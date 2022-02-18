940eb339f07298929c36998180ffdc429105988b
Charles Harries <charles@harries.me>
2022-02-18 20:37:52 +0000

    Add images to flexible milestone response

--- modules/helpers/models/Project.php
+++ modules/helpers/models/Project.php
@@ -4,19 +4,26 @@ namespace helpers\models;
 
 use craft\elements\Entry;
 use helpers\spotify\Track;
+use helpers\traits\HasImages;
+use mmikkel\retcon\Retcon;
 
 class Project
 {
+    use HasImages;
+
     public static function transform(Entry $entry)
     {
         $getTrack = fn ($track) => (new Track($track['id']))->transform();
 
         $body = array_map(function ($block) use ($getTrack) {
+            $image = '<img src="' . $block->image->one()->getUrl() . '" alt="' . $block->image->one()->title . '" />';
+
             return [
                 'heading' => $block->heading,
                 'year' => $block->year,
                 'body' => $block->body,
                 'tracks' => array_map($getTrack, $block->songs),
+                'image' => Retcon::$plugin->retcon->srcset($image, self::widths()),
             ];
         }, $entry->flexibleContent->all());
 
