b33721cf64ca9554a3aff946c005551e1e52eab9
Charles Harries <charles@harries.me>
2022-08-11 22:11:55 +0100

    Lightbox for images
    Closes #12

---
+++ web/js/lightbox.js
@@ -0,0 +1,41 @@
+function Lightbox() {
+    const images = document.querySelectorAll('img:not(.no-lightbox)');
+    const lightbox = document.getElementById('lightbox');
+
+    function clearLightbox() {
+        lightbox.innerHTML = '';
+    }
+
+    function openLightbox(event) {
+        clearLightbox();
+        console.log('opening lightbox');
+
+        const img = document.createElement('img');
+        img.src = event.target.src;
+        img.alt = event.target.alt;
+        lightbox.appendChild(img);
+        
+        lightbox.style.display = 'block';
+    }
+
+    function preloadLightbox(event) {
+        const img = document.createElement('img');
+        img.src = event.target.src;
+    }
+
+    function closeLightbox(event) {
+        clearLightbox();
+        lightbox.style.display = 'none';
+    }
+
+    function handleKeydown(event) {
+        event.key === 'Escape' && closeLightbox();
+    }
+
+    images.forEach((img) => img.addEventListener('click', openLightbox));
+    images.forEach((img) => img.addEventListener('hover', preloadLightbox));
+    lightbox.addEventListener('click', closeLightbox)
+    window.addEventListener('keydown', handleKeydown);
+}
+
+Lightbox();
