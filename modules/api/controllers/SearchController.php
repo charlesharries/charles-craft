290eed90c31d37dad3feb78dd565c52c1d1e4aaf
Charles Harries <charles@harries.me>
2023-01-25 10:57:55 +0000

    Create SearchResult class
    So that we're not returning massive gobs of JSON on every search

--- modules/api/controllers/SearchController.php
+++ modules/api/controllers/SearchController.php
@@ -4,9 +4,54 @@ namespace modules\api\controllers;
 
 use Craft;
 use craft\elements\Entry;
+use craft\elements\Tag;
 use craft\web\Controller;
+use DateTime;
+use Illuminate\Support\Collection;
+use mmikkel\retcon\library\RetconDom;
 use yii\web\NotFoundHttpException;
 use yii\web\Response;
+
+class SearchResult
+{
+    public string $title;
+    public string $postDate;
+    public string $url;
+    public string $summary;
+    public string $result;
+    private string $searchTerm;
+    public Collection $tags;
+
+    public function __construct(Entry $entry, string $searchTerm)
+    {
+        $this->title = $entry->title;
+        $this->postDate = $entry->postDate->format("l, j M Y g:i a");
+        $this->url = $entry->url;
+        $this->summary = $entry->summary;
+        $this->searchTerm = $searchTerm;
+        $this->tags = $this->getTags($entry);
+        $this->highlight($entry->body);
+    }
+
+    private function highlight(string $body)
+    {
+        $dom = new RetconDom($body);
+        $elements = $dom->filter('p, blockquote, ul, ol');
+        foreach ($elements as $element) {
+            if (\str_contains($element->textContent, $this->searchTerm)) {
+                $this->result = $element->textContent;
+                return;
+            }
+        }
+    }
+
+    private function getTags(Entry $entry)
+    {
+        return (new Collection($entry->tags->all()))->map(function (Tag $tag) {
+            return $tag->title;
+        });
+    }
+}
 
 class SearchController extends Controller
 {
@@ -14,8 +59,10 @@ class SearchController extends Controller
 
     public function actionGet($q): Response
     {
-        $entries = Entry::find()->search($q)->all();
+        $entries = new Collection(Entry::find()->search($q)->all());
 
-        return $this->asJson($entries);
+        return $this->asJson($entries->map(function (Entry $entry) use ($q) {
+            return new SearchResult($entry, $q);
+        }));
     }
 }
