568d6dbedba09e24bd98dc8491671743e0021f63
Charles Harries <charles@labs.charlesharri.es>
2023-10-16 12:25:36 +0000

    Project config updates

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ VERSION=$(openssl rand -hex 4)
 git checkout -- ./templates
 git pull origin main
 composer install
-grep -rl '{|{VERSION}|}' ./templates | xargs sed -i 's/{|{VERSION}|}/$VERSION/g'
+grep -rl '{|{VERSION}|}' ./templates | xargs sed -i "s/{|{VERSION}|}/$VERSION/g"
