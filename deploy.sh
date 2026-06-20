ef1bb7e34f39a98ca1880ce54741d6d4b08c3eb3
Charles Harries <charles@harries.me>
2026-06-20 15:32:36 +0000

    Updates to deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ VERSION=$(openssl rand -hex 4)
 git checkout HEAD -- ./templates
 git pull origin main
 composer install
+php craft migrate/all --interactive 0
+php craft project-config/apply
 grep -rl '{|{VERSION}|}' ./templates | xargs sed -i "s/{|{VERSION}|}/$VERSION/g"
