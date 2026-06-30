6522b296e9a1fa966f279a7fd2f0a25938099d1f
Charles Harries <charles@harries.me>
2026-06-30 23:03:37 +0100

    Don't install dev as part of deployment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 VERSION=$(openssl rand -hex 4)
 git checkout HEAD -- ./templates
 git pull origin main
-composer install
+composer install --no-dev
 php craft migrate/all --interactive 0
 php craft project-config/apply
 grep -rl '{|{VERSION}|}' ./templates | xargs sed -i "s/{|{VERSION}|}/$VERSION/g"
